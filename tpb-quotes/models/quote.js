d13eecce892aa57f258742bf0a6a144480a4642f
rela7e <pdarcy@riseup.net>
2024-04-04 16:16:00 -0400

    add character and quote schema

---
+++ tpb-quotes/models/quote.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const quoteSchema = new Schema({
+    quote: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Quote', quoteSchema);
