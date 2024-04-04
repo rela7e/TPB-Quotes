9d007ac458236c69702929d305b3ccb59031117f
rela7e <pdarcy@riseup.net>
2024-04-04 18:44:09 -0400

    create quote show function

--- tpb-quotes/controllers/quotes.js
+++ tpb-quotes/controllers/quotes.js
@@ -6,3 +6,8 @@ async function index(req, res) {
     res.render('quotes/index', {title: 'All Quotes', quotes });
 }
 
+async function show(req, res) {
+    const character = await Character.findById(req.params.id)
+    res.render('quotes/show', { title: 'Quote', quote })
+}
+
