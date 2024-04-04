8be6445ac93474a055996bb183ed0131b8b29765
rela7e <pdarcy@riseup.net>
2024-04-04 18:43:05 -0400

    create quote index route

--- tpb-quotes/controllers/quotes.js
+++ tpb-quotes/controllers/quotes.js
@@ -1,3 +1,8 @@
 const Character = require('../models/character');
 const Quote = require('../models/quote');
 
+async function index(req, res) {
+    const quotes = await Quote.find({});
+    res.render('quotes/index', {title: 'All Quotes', quotes });
+}
+
